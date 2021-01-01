b34972f0978824fb92737b76f72a8a4e239dcfdd
Halbert Lei <princescar@gmail.com>
2021-01-01 23:57:37 +0800

    upgrade deps.

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,6 @@ module.exports = {
   optimization: {
     minimizer: [
       new TerserPlugin({
-        sourceMap: true,
         terserOptions: {
           output: {
             comments: false,
